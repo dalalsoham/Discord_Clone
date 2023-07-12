413bc645b3a36133f4d5c5958f4e49a0646a0640
mr_sd_jack_003 <barson0habra@gmail.com>
2023-07-12 23:53:42 +0530

    half done sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import './Sidebar.css';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+import SidebarChannel from './SidebarChannel';
+import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h2>I am the Sidebar</h2>
-
       <div className="sidebar__top">
         <h3>Soham Server</h3>
         <ExpandMoreIcon />
+      </div>
+
+      <div className="sidebar__channels">
+        <div className="sidebar__channelsHeader">
+            <div className="sidebar__header">
+                <ExpandMoreIcon />
+                <h4>Text Channels</h4>
+            </div>
+
+            <AddIcon className= "sidebar__addChannel" />
+        </div>
+
+        <div className="sidebar__channelsList">
+        <SidebarChannel />
+        <SidebarChannel />
+        <SidebarChannel />
+      </div>
+      </div>
+
+      <div className="sidebar__voice">
+        <SignalCellularAltIcon 
+            className="sidebar__voiceIcon"
+            fontSize="large"
+        />
+        <div className="sidebar__voiceInfo">
+            <h3>Voice Connected</h3>
+            <p>Stream</p>
+        </div>
+
+        <div className="sidebar__voiceIcons">
+            <InfoOutlinedIcon />
+            <CallIcon />
+        </div>
       </div>
     </div>
   );
