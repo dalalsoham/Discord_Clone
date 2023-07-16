5a7dc706bf0a754c9105a27781453b70050f559f
mr_sd_jack_003 <barson0habra@gmail.com>
2023-07-16 20:51:37 +0530

    update favicon and login problem

--- src/Login.js
+++ src/Login.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './Login.css';
 import { Button } from '@mui/material';
-import { auth, provider } from './firebase';
+// import { auth, provider } from './firebase';
 
 function Login() {
     const signIn = () => {
         //google login here
-        auth.signInWithPopup(provider).catch((error) => alert(error.message));
+        // auth.signInWithPopup(provider).catch((error) => alert(error.message));
     };
 
   return (
     <div className="login">
 
       <div className="login__logo">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcRDaOSj1l8P00HCKZ7Tl5NA5Z6E2cvG62qQ&usqp=CAU" />
+        <img src="https://1000logos.net/wp-content/uploads/2021/06/Discord-logo.png" />
       </div>
 
       <Button onClick={signIn}>Sign In</Button>
