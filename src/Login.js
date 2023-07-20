a6d28dc1bc6a476e5738a88bb52ef3accfb067af
mr_sd_jack_003 <barson0habra@gmail.com>
2023-07-21 00:58:13 +0530

    UPDATE auth "error"

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';//UPDATE
 import './Login.css';
 import { Button } from '@mui/material';
-import { auth, provider } from './firebase';
+// import { auth, provider } from './firebase';
+import { auth,provider } from './config';//UPDATE
+import {signInWithPopup} from "firebase/auth";//UPDATE
 
 function Login() {
     const signIn = () => {
+      //UPDATE CODE START
         //google login here
-        auth.signInWithPopup(provider).catch((error) => alert(error.message));
+        const [value,setvalue] = useState('')
+        // auth.signInWithPopup(provider).catch((error) => alert(error.message));
+        signInWithPopup(auth,provider).then((data) =>{
+          setvalue(data.user.email);
+          localStorage.setItem("email", data.user.email);
+
+        })
+        //UPDATE CODE END
     };
+//UPDATE CODE
+    useState(() => {
+      setvalue(localStorage.getItem('email'));
+    })
+    //UPDATE CODE END
 
   return (
     <div className="login">
@@ -16,6 +31,7 @@ function Login() {
         <img src="https://1000logos.net/wp-content/uploads/2021/06/Discord-logo.png" />
       </div>
 
+      {/* {value?} */}
       <Button onClick={signIn}>Sign In</Button>
     </div>
   );
